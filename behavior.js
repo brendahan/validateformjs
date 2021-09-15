922888d8929f1b4836467328fa46dfd1f1d74207
Brendahan <2010.tezlove@gmail.com>
2021-09-15 18:41:55 +0300

    Validate input fields

--- behavior.js
+++ behavior.js
@@ -1,86 +1,44 @@
-//Defining function
-function formValidation() {
-    var firstName = document.regform.fname;
-    var lastName = document.regform.lname;
-    var email = document.regform.email;
-    var filter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+&/;
-    var email = document.regform.email;
-    var tel = document.regform.tele;
-    var origin = document.regform.origin;
-    var age = document.regform.age;
-    var sex =document.regform.gender;
-    if (fname_validation(firstName, 3, 10)) {
-    }
-    if (lname_validation(lastName, 3, 10)) {
-    }
-    if (! filter.test(email.value)){
-    }
-     if (allnumeric(tel)){
-    }
-    if (countryselect(origin)){
-    }
-    if(numeric(age)){
-    }
-    if (genderselect(sex)){
-    }
-    return false;
+
+function validateForm(){
+  var firstName = document.forms["regForm"]["fname"].value;
+  var lastName = document.forms["regForm"]["lname"].value;
+var email = document.forms["regForm"]["email"].value;
+var tel= document.forms["regForm"]["phone"].value;
+var origin = document.forms["regForm"]["origin"].value;
+  var age = document.forms["regForm"]["age"].value;
+var gender= document.forms["regForm"]["gender"].value;
+
+
+if (firstName.length<1) {
+      document.getElementById('errorFirstName').innerHTML = " Please Enter Your first name *";
+      errorFirstName.style.color = 'red';
   }
-  console.log("almost getting it")
-  
-
-  function fname_validation(firstName, mx, my) {
-    var firstName_len = firstName.value.length;
-    if (firstName_len == 0 || firstName_len >= my || firstName_len < mx) {
-      alert("Firstname has error" + " " + mx + " to " + my );
-      firstName.style.border = '3px solid red';
-      firstName.focus();
-    }
-    return true;
-}
-
-  function lname_validation(lastName, mx, my) {
-    var lastName_len = lastName.value.length;
-    if (lastName_len == 0 || lastName_len >= my || lastName_len < mx) {
-      alert("Lastname has error" + " " + mx + " to " + my);
-      lastName.style.border = '3px solid red';
-      lastName.focus();
-    }
-    return true;
-}
-
-function email_validation(email){
-  if (! filter.test(email.value)){
-      alert("Please provide a valid email address");
-      email.focus();
-      return false;
-    }
-}
-
-  function allnumeric(tel) {
-    var numbers = /^[0-9]+$/;
-    if (tel.value.match(numbers)) {
-      return true;
-    } else {
-      alert("Please enter a number");
-      return false;
-    }
+  if (lastName.length<1) {
+      document.getElementById('errorLastName').innerHTML = " Please Enter Your last name *";
+      errorLastName.style.color = 'red';
   }
-  function numeric(age) {
-    var numbers = /^[0-9]+$/;
-    if (age.value.match(numbers)) {
-      return true;
-    } else {
-      alert("Please enter a number");
-      return false;
-    }
+  if (email.length<1) {
+      document.getElementById('errorEmail').innerHTML = " Please Enter Your last name *";
+      errorEmail.style.color = 'red';
   }
-
-  function origin_validation(origin){
-      if (formValidation.origin.value == 'Default'){
-          alert('Please make a selection');
-          formValidation.origin.focus();
-          return false;
-      } else {
-        return true;
-      }
-  } 
+  if (tel.length<1) {
+      document.getElementById('errorPhone').innerHTML = " Please Enter Your Phonenumber *";   
+      errorPhone.style.color = 'red';   
+  }
+  if (age.length<1) {
+      document.getElementById('errorAge').innerHTML = " Please Enter Your age *"; 
+      errorAge.style.color = 'red';     
+  }
+  if (origin.length<1) {
+      document.getElementById('errorOrigin').innerHTML = " Please select Your origin *";
+      errorOrigin.style.color = 'red';
+  }          
+  if (gender.length<1) {
+      document.getElementById('errorGender').innerHTML = " Please Select your gender *"; 
+      errorGender.style.color = 'red';     
+  }
+  if(firstName.length<1 || lastName.length<1 || email.length<1 || tel.length<1 || origin.length<1 || age.length<1 || gender.length<1 ){
+       return false;
+  }            
+}
+console.log("almost there");
